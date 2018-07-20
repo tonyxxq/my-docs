73614a83d011e6cc8540e42fac9fceb3e2fafd15
Hynek Schlindenbuch <schlindebuch.h@seznam.cz>
2018-07-20 13:26:15 +0200

    fix tests

--- src/test/java/com/ggp/games/TicTacToe/CompleteInformationStateTest.java
+++ src/test/java/com/ggp/games/TicTacToe/CompleteInformationStateTest.java
@@ -39,21 +39,21 @@ public class CompleteInformationStateTest {
     public void testGameplay_xWin_col() {
         CompleteInformationState s = initialState;
         assertSame(25, s.getLegalActions().size());
-        s = (CompleteInformationState) s.next(MarkFieldAction.getAction(1, 0, 0));
+        s = (CompleteInformationState) s.next(MarkFieldAction.Cache.getAction(1, 0, 0));
         assertSame(2, s.getActingPlayerId());
         assertSame(25, s.getLegalActions().size());
         // test percepts
-        assertPercept(s.getPercepts(MarkFieldAction.getAction(2, 0, 0)), false, 2);
+        assertPercept(s.getPercepts(MarkFieldAction.Cache.getAction(2, 0, 0)), false, 2);
         for (int x = 0; x < 5; ++x) {
             for (int y = 0; y < 5; ++y) {
                 if (x == 0 && y == 0) continue;
-                assertPercept(s.getPercepts(MarkFieldAction.getAction(2, x, y)), true, 2);
+                assertPercept(s.getPercepts(MarkFieldAction.Cache.getAction(2, x, y)), true, 2);
             }
         }
 
-        s = (CompleteInformationState) s.next(MarkFieldAction.getAction(2, 0, 0));
-        assertFalse(s.isLegal(MarkFieldAction.getAction(2, 0, 0))); // wrong player
-        assertFalse(s.isLegal(MarkFieldAction.getAction(1, 0, 0))); // already marked
+        s = (CompleteInformationState) s.next(MarkFieldAction.Cache.getAction(2, 0, 0));
+        assertFalse(s.isLegal(MarkFieldAction.Cache.getAction(2, 0, 0))); // wrong player
+        assertFalse(s.isLegal(MarkFieldAction.Cache.getAction(1, 0, 0))); // already marked
         assertFalse(s.isTerminal());
         CompleteInformationState lastRound = null;
         for (int x = 1; x < 5; ++x) {
@@ -61,9 +61,9 @@ public class CompleteInformationStateTest {
             if (x == 4) {
                 lastRound = s;
             }
-            s = (CompleteInformationState) s.next(MarkFieldAction.getAction(1, x, 0));
+            s = (CompleteInformationState) s.next(MarkFieldAction.Cache.getAction(1, x, 0));
             assertFalse(s.isTerminal());
-            s = (CompleteInformationState) s.next(MarkFieldAction.getAction(2, x, 1));
+            s = (CompleteInformationState) s.next(MarkFieldAction.Cache.getAction(2, x, 1));
         }
         assertTrue(s.isTerminal());
         // player 1 wins
@@ -71,10 +71,10 @@ public class CompleteInformationStateTest {
         assertSame(-1, (int) s.getPayoff(2));
 
         // make player 2 win starting from lastRound
-        s = (CompleteInformationState) lastRound.next(MarkFieldAction.getAction(1, 1, 1));
-        s = (CompleteInformationState) s.next(MarkFieldAction.getAction(2, 4, 1));
-        s = (CompleteInformationState) s.next(MarkFieldAction.getAction(1, 2, 1));
-        s = (CompleteInformationState) s.next(MarkFieldAction.getAction(2, 0, 1));
+        s = (CompleteInformationState) lastRound.next(MarkFieldAction.Cache.getAction(1, 1, 1));
+        s = (CompleteInformationState) s.next(MarkFieldAction.Cache.getAction(2, 4, 1));
+        s = (CompleteInformationState) s.next(MarkFieldAction.Cache.getAction(1, 2, 1));
+        s = (CompleteInformationState) s.next(MarkFieldAction.Cache.getAction(2, 0, 1));
         assertTrue(s.isTerminal());
         // player 2 wins
         assertSame(-1, (int) s.getPayoff(1));
@@ -85,8 +85,8 @@ public class CompleteInformationStateTest {
     public void testGameplay_xWin_row() {
         CompleteInformationState s = initialState;
         for (int y = 0; y < 5; ++y) {
-            s = (CompleteInformationState) s.next(MarkFieldAction.getAction(1, 2, y));
-            s = (CompleteInformationState) s.next(MarkFieldAction.getAction(2, y, 0));
+            s = (CompleteInformationState) s.next(MarkFieldAction.Cache.getAction(1, 2, y));
+            s = (CompleteInformationState) s.next(MarkFieldAction.Cache.getAction(2, y, 0));
         }
 
         assertTrue(s.isTerminal());
@@ -99,8 +99,8 @@ public class CompleteInformationStateTest {
     public void testGameplay_oWin_diag() {
         CompleteInformationState s = initialState;
         for (int y = 0; y < 5; ++y) {
-            s = (CompleteInformationState) s.next(MarkFieldAction.getAction(1, 0, 4-y));
-            s = (CompleteInformationState) s.next(MarkFieldAction.getAction(2, y, y)); // player 2 will reach [0,0] first
+            s = (CompleteInformationState) s.next(MarkFieldAction.Cache.getAction(1, 0, 4-y));
+            s = (CompleteInformationState) s.next(MarkFieldAction.Cache.getAction(2, y, y)); // player 2 will reach [0,0] first
         }
 
         assertTrue(s.isTerminal());
@@ -113,9 +113,9 @@ public class CompleteInformationStateTest {
     public void testGameplay_draw() {
         CompleteInformationState s = initialState;
         for (int y = 0; y < 5; ++y) {
-            s = (CompleteInformationState) s.next(MarkFieldAction.getAction(1, 0, y));
+            s = (CompleteInformationState) s.next(MarkFieldAction.Cache.getAction(1, 0, y));
             assertFalse(s.isTerminal());
-            s = (CompleteInformationState) s.next(MarkFieldAction.getAction(2, 1, y));
+            s = (CompleteInformationState) s.next(MarkFieldAction.Cache.getAction(2, 1, y));
         }
 
         assertTrue(s.isTerminal());
