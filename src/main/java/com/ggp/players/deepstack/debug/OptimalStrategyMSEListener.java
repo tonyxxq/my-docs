ba7b88b3b116065ca77f5623ec5eb369e846b7e8
Hynek Schlindenbuch <schlindebuch.h@seznam.cz>
2018-09-16 14:39:32 +0200

    use BaseListener in OptimalStrategyMSEListener

--- src/main/java/com/ggp/players/deepstack/debug/OptimalStrategyMSEListener.java
+++ src/main/java/com/ggp/players/deepstack/debug/OptimalStrategyMSEListener.java
@@ -1,24 +1,16 @@
 package com.ggp.players.deepstack.debug;
 
-import com.ggp.ICompleteInformationState;
 import com.ggp.players.deepstack.IResolvingInfo;
-import com.ggp.players.deepstack.IResolvingListener;
 import com.ggp.players.deepstack.utils.Strategy;
 import com.ggp.utils.Metrics;
 
-public class OptimalStrategyMSEListener implements IResolvingListener {
+public class OptimalStrategyMSEListener extends BaseListener {
     private Strategy optimal;
     private int skipIterations = 5000;
     private int currentIteration;
-    private boolean initEnded = false;
 
     public OptimalStrategyMSEListener(Strategy optimal) {
         this.optimal = optimal;
-    }
-
-    @Override
-    public void initEnd(IResolvingInfo resInfo) {
-        initEnded = true;
     }
 
     @Override
@@ -27,18 +19,8 @@ public class OptimalStrategyMSEListener implements IResolvingListener {
     }
 
     @Override
-    public void resolvingEnd(IResolvingInfo resInfo) {
-
-    }
-
-    @Override
-    public void stateVisited(ICompleteInformationState s, IResolvingInfo resInfo) {
-
-    }
-
-    @Override
     public void resolvingIterationEnd(IResolvingInfo resInfo) {
-        if (!initEnded) return;
+        if (!hasInitEnded()) return;
         currentIteration++;
         if (currentIteration % skipIterations == 1) {
             System.out.println(String.format("Optimal strategy MSE: %e", Metrics.getStrategyMSE(optimal, resInfo.getUnnormalizedCumulativeStrategy())));
