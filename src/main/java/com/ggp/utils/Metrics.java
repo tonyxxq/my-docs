57853b5d1551bd8bbe23d2782940bb8db3df1dd5
Hynek Schlindenbuch <schlindebuch.h@seznam.cz>
2018-10-18 10:05:32 +0200

    add strategy MSE computation for 1 info set

--- src/main/java/com/ggp/utils/Metrics.java
+++ src/main/java/com/ggp/utils/Metrics.java
@@ -7,26 +7,32 @@ import com.ggp.IStrategy;
 import java.util.List;
 
 public class Metrics {
+    public static double getStrategyMSE(IStrategy normalizedTarget, IStrategy unnormalizedCurrent, IInformationSet is) {
+        double errSum = 0;
+        List<IAction> legalActions = is.getLegalActions();
+        if (legalActions == null || legalActions.isEmpty()) return errSum;
+        double total = 0;
+        for (IAction a: legalActions) {
+            total += unnormalizedCurrent.getProbability(is, a);
+        }
+        for (IAction a: legalActions) {
+            double diff = normalizedTarget.getProbability(is, a);
+            if (total > 0) {
+                diff -= unnormalizedCurrent.getProbability(is, a)/total;
+            } else {
+                diff -= 1d/legalActions.size();
+            }
+            errSum += diff*diff;
+        }
+        return errSum;
+    }
+
     public static double getStrategyMSE(IStrategy normalizedTarget, IStrategy unnormalizedCurrent) {
         int isCount = 0;
         double errSum = 0;
         for (IInformationSet is: unnormalizedCurrent.getDefinedInformationSets()) {
-            List<IAction> legalActions = is.getLegalActions();
-            if (legalActions == null || legalActions.isEmpty()) continue;
             isCount++;
-            double total = 0;
-            for (IAction a: legalActions) {
-                total += unnormalizedCurrent.getProbability(is, a);
-            }
-            for (IAction a: legalActions) {
-                double diff = normalizedTarget.getProbability(is, a);
-                if (total > 0) {
-                    diff -= unnormalizedCurrent.getProbability(is, a)/total;
-                } else {
-                    diff -= 1d/legalActions.size();
-                }
-                errSum += diff*diff;
-            }
+            errSum += getStrategyMSE(normalizedTarget, unnormalizedCurrent, is);
         }
         if (isCount > 0) return errSum/isCount;
         return 0;
